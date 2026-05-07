904d5856f6aa1d5b417a229581667db0c1b88b6a
Roz Hencinski <rosamundhencinski@gmail.com>
2026-05-07 19:59:16 -0400

    Pause point on project, backing up to learning git and OOP

---
+++ player.py
@@ -0,0 +1,16 @@
+from circleshape import *
+from constants import *
+class Player(CircleShape):
+    #constructor
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
